05e5621c8a86dbdd50e734b6248a1a7ac4d5f39a
Sarp User <sarpuser@gmail.com>
2024-11-25 14:24:51 -0600

    2023/day7 complete

---
+++ 2023/day7/part2.py
@@ -0,0 +1,73 @@
+from collections import Counter
+
+winnings = 0
+hands = []
+bids = []
+handStrengths = []
+
+def replaceJokers (hand: str) -> str:
+	if 'J' not in hand or hand == 'JJJJJ':
+		return hand
+
+	counts = Counter(hand)
+	countsSorted = counts.most_common(2)
+	bestCard = countsSorted[0][0] if countsSorted[0][0] != 'J' else countsSorted[1][0]
+	return hand.replace('J', bestCard)
+
+
+def getHandType (hand: str) -> str:
+	counts = Counter(hand)
+	if 5 in counts.values():
+		# Five of a kind
+		return '6'
+	elif 4 in counts.values():
+		# Four of a kind
+		return '5'
+	elif 3 in counts.values() and 2 in counts.values():
+		# Full house
+		return '4'
+	elif 3 in counts.values():
+		# Three of a kind
+		return '3'
+	elif 2 in counts.values() and Counter(counts.values())[2] == 2:
+		# Two pair
+		return '2'
+	elif 2 in counts.values() and Counter(counts.values())[2] == 1:
+		# One pair
+		return '1'
+	else:
+		# High card
+		return '0'
+
+
+with open('input.txt', 'r') as fin:
+	for line in fin.readlines():
+		lineItems = line.split(' ')
+		hand = lineItems[0]
+		bid = int(lineItems[1].replace('\n', ''))
+		hands.append(hand)
+		bids.append(bid)
+
+		# Replace all the letters with their rank in hex, A -> 13 (D), K -> 12 (C)...
+		handStrength = hand.replace('A', 'E')
+		handStrength = handStrength.replace('K', 'D')
+		handStrength = handStrength.replace('Q', 'C')
+		handStrength = handStrength.replace('T', 'A')
+		handStrength = handStrength.replace('J', '0')
+
+		# Hand type is the most important so we add it to the beginning of handStrength
+		handStrength = getHandType(replaceJokers(hand)) + handStrength
+
+		handStrengths.append(int(handStrength, base=16))
+
+# Sort hands and bids according to the handStrengths
+handsSorted = [hand for _, hand in sorted(zip(handStrengths, hands))]
+bidsSorted = [bid for _, bid in sorted(zip(handStrengths, bids))]
+handStrengthsSorted = sorted(handStrengths)
+
+for bidIndex, bid in enumerate(bidsSorted):
+	handWinnings = (bidIndex + 1) * bid
+	winnings += handWinnings
+	print(handsSorted[bidIndex], bid, handWinnings, winnings)
+
+# Passed! - 248909434
