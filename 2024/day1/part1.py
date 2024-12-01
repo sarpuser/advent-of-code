5c023dba759ca965da2d9d75776bb7a2b13b4791
Sarp User <sarpuser@gmail.com>
2024-12-01 00:12:07 -0500

    2024/day1/part1 complete

---
+++ 2024/day1/part1.py
@@ -0,0 +1,20 @@
+import re
+
+leftNums = []
+rightNums = []
+sum = 0
+
+with open('input.txt', 'r') as fin:
+	for line in fin.readlines():
+		lineList = re.findall('(\d+)', line)
+		leftNums.append(int(lineList[0]))
+		rightNums.append(int(lineList[1]))
+
+leftNumsSorted = sorted(leftNums)
+rightNumsSorted = sorted(rightNums)
+
+for i, num in enumerate(leftNumsSorted):
+	sum += abs(num - rightNumsSorted[i])
+	print(sum)
+
+# Passed! - 2164381
